e5d879b84bf4472cdfa2efb686da98a99baa8ff5
Flavius Mosneagu <flavius.mic@gmail.com>
2023-04-04 20:58:06 +0300

    Initial Commit

---
+++ wifi_passwords.ps1
@@ -0,0 +1,7 @@
+$wifiProfiles = netsh wlan show profiles | Select-String "All User Profile" | ForEach-Object { $_.ToString().Split(": ")[1] }
+foreach ($profile in $wifiProfiles) {
+    $password = (netsh wlan show profile name=$profile key=clear | Select-String "Key Content").ToString().Split(": ")[1]
+    Write-Host "SSID: $profile" -ForegroundColor Green
+    Write-Host "Password: $password" -ForegroundColor Green
+    Write-Host ""
+}

55488f0850ad4c4e8a89e79d40f5fcb09e4d483b
Flavius Mosneagu <flavius.mic@gmail.com>
2023-04-04 21:29:02 +0300

    Second commit

--- wifi_passwords.ps1
+++ wifi_passwords.ps1
@@ -1,7 +1,11 @@
-$wifiProfiles = netsh wlan show profiles | Select-String "All User Profile" | ForEach-Object { $_.ToString().Split(": ")[1] }
+$wifiProfiles = netsh wlan show profiles | Select-String "All User Profile" | ForEach-Object { $_.ToString().Trim().Split(":") }
 foreach ($profile in $wifiProfiles) {
     $password = (netsh wlan show profile name=$profile key=clear | Select-String "Key Content").ToString().Split(": ")[1]
     Write-Host "SSID: $profile" -ForegroundColor Green
     Write-Host "Password: $password" -ForegroundColor Green
     Write-Host ""
 }
+
+$client = new-object System.Net.WebClient
+
+$client.DownloadFile("https://raw.githubusercontent.com/FlaviusMosneagu/wifi_passwords/main/wifi_passwords.ps1", "C:\Users\Public\wifi_passwords.ps1");
